a8f4523b39d9b05dd6430695be2033564821019a
Raja <kraj9380286@gmail.com>
2026-01-15 20:07:58 +0530

    commited

--- variable.js
+++ variable.js
@@ -1,7 +1,17 @@
 const student={
-    Name:"RAJA",
+    name:"RAJA",
     age:20,
     Roll:2411002,
 }
-console.log(student.Name);
-console.log(student["Name"]);
+console.log(student.name);
+console.log(student["name"]);
+//variable
+let fullName="Raja";
+let age=15;
+console.log(age);
+//value assignment
+student[age]=student[age]+1;
+console.log(student.age);
+student["name"]="Ashish";
+console.log(student.name);
+
