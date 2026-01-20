4012bef80376ad4a740c8e3491d083efd48bc08c
Raja <kraj9380286@gmail.com>
2026-01-20 20:15:53 +0530

    commited

--- Functions.js
+++ Functions.js
@@ -5,8 +5,12 @@
 //  myfunction();
 
 /* Arrow Function*/
- const Arrsum=(a,b)=>{
-    console.log(a+b);
- };
+//  const Arrsum=(a,b)=>{
+//     console.log(a+b);
+//  };
 
- Arrsum(7,3);
+//  Arrsum(7,3);
+
+ const printHell0=()=>{
+  console.log("Hello friends");
+ }
