eb362bc99f9e18c35db41963e20524b8c67cc8db
Raja <kraj9380286@gmail.com>
2026-01-20 09:19:52 +0530

    commited

--- Loops.js
+++ Loops.js
@@ -1,4 +1,13 @@
+//For-of interater
+// let str="RajaKumarSaniya";
+// for(let i of str){
+//     console.log("i=",i);
+// }
+//Length
 let str="RajaKumarSaniya";
+let size=0;
 for(let i of str){
     console.log("i=",i);
+    size++;
 }
+console.log("Size=",size);
