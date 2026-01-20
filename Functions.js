5515651255677013bb0f1c250d53d0239099f0ad
Raja <kraj9380286@gmail.com>
2026-01-20 20:32:02 +0530

    commited

--- Functions.js
+++ Functions.js
@@ -15,20 +15,36 @@
 //   console.log("Hello friends");
 //  }
 
-      /*1.Practice Question*/  // Hello ->vowels->2
-      
-  function countVowel(val) {
-    let count = 0;
-    let vowel = ['A','E','I','O','U','a','e','i','o','u'];
 
-    for (let i of val) {
-        for (let j of vowel) {
-            if (i === j) {
+
+      /*1.Practice Question*/  // Hello ->vowels->2
+
+     // methode 1
+      
+//   function countVowel(val) {
+//     let count = 0;
+//     let vowel = ['A','E','I','O','U','a','e','i','o','u'];
+
+//     for (let i of val) {
+//         for (let j of vowel) {
+//             if (i === j) {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countVowel("hello")); // 2
+    //Methode 2
+    function countVowel(val){
+        let count=0;
+        let vowel="AEIOUaeiou";
+        for(let i of val){
+            if(vowel.includes(i)){
                 count++;
             }
         }
+        return count;
     }
-    return count;
-}
-
-console.log(countVowel("hello")); // 2
+    console.log(countVowel("Hello"));
