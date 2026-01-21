ef56b26e7fc58bd3d4c394d952daaa802c0a2f0c
Raja <kraj9380286@gmail.com>
2026-01-21 19:15:54 +0530

    commited

--- Events.js
+++ Events.js
@@ -9,10 +9,34 @@ let div=document.querySelector("#parentBox");
 
 
       //Used for adding Multiple Events
-  div.addEventListener("click",()=>{
-    console.log("Hello i am here inside Div");
-  });
+
+
+//   div.addEventListener("click",()=>{
+//     console.log("Hello i am here inside Div");
+//   });
   
-  div.addEventListener("click",()=>{
-    console.log("Hello we are here inside Div");
-  });
+//   div.addEventListener("click",()=>{
+//     console.log("Hello we are here inside Div");
+//   });
+
+     /*Remove AddeventLisner*/
+  //store for removing Process
+const Handler=()=>{
+     console.log("Handler 3");
+}
+
+   div.addEventListener("click",()=>{
+    console.log("Handler 1");
+      });
+
+      div.addEventListener("click",()=>{
+    console.log("Handler 2");
+      });
+      div.addEventListener("click","Handler");
+      div.addEventListener("click",()=>{
+    console.log("Handler 4");
+      });
+
+      div.removeEventListener("click",'Handler');
+
+
