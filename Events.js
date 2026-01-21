9737ab851bee3e602d9b130ffb689b73f53c6051
Raja <kraj9380286@gmail.com>
2026-01-21 19:06:41 +0530

    commited

--- Events.js
+++ Events.js
@@ -3,6 +3,16 @@ let div=document.querySelector("#parentBox");
 //     console.log("Yes i am here 2");
 // }
 
-div.onmouseover=()=>{
-    console.log('You are inside the div');
-}
+// div.onmouseover=()=>{
+//     console.log('You are inside the div');
+// }
+
+
+      //Used for adding Multiple Events
+  div.addEventListener("click",()=>{
+    console.log("Hello i am here inside Div");
+  });
+  
+  div.addEventListener("click",()=>{
+    console.log("Hello we are here inside Div");
+  });
